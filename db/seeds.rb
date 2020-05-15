1c4e8c3970a5f84228d6b1340bc0723c45f05e3f
s3528350 <s3528350@student.rmit.edu.au>
2020-05-15 02:38:59 +0000

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+User.create!(name: "Example User", email: "example@railstutorial.org", password: "foobar", password_confirmation: "foobar", admin: true, activated: true, activated_at: Time.zone.now)
+User.create!(name: "Test User", email: "test@railstutorial.org", password: "foobar", password_confirmation: "foobar", activated: true, activated_at: Time.zone.now)
+
+99.times do |n|
+    name = Faker::Name.name
+    email = "example-#{n+1}@railstutorial.org"
+    password = "password"
+    User.create!(name: name, email: email, password: password, password_confirmation: password, activated: true, activated_at: Time.zone.now)
+end
